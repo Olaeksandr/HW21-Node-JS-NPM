fd12d006b453aa0e07e3861755ba45519d17c5c6
Oleksandr Masiuk <ommasiuk@gmail.com>
2020-03-02 05:28:13 +0200

    first version

---
+++ app.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator');
+
+console.log('2+3=', calculator.add(2,3));
+console.log('2-3=', calculator.sub(2,3));
+console.log('2*3=', calculator.mult(2,3));
+console.log('6/3=', calculator.div(6,3));
